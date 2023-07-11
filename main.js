34335c40162ff766eee2a9c215d779b324404f27
AMNA-55 <122812255+AMNA-55@users.noreply.github.com>
2023-07-11 23:13:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river5 = document.getElementById('river5');
+let boat6 = document.getElementById('boat6');
+let Nouvil = document.querySelector('.Nouvil');
+window.onscroll = function () {
+    let value = scrollY ; 
+    stars.style.left = value + 'px' ;
+}
